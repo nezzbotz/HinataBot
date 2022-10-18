5f6e3cdcf4bc533feceda173d80e08e94c57a300
Meong Sad <113411107+nezzbotz@users.noreply.github.com>
2022-10-19 06:11:31 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,1 +0,0 @@
-rm -rf HinataBot && git clone https://github.com/AyGemuy/HinataBot && sudo apt install ffmpeg imagemagick -y && cd HinataBot && npm install && npm install -g pm2 && pm2 start index.js && pm2 logs
